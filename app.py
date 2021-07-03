ee019d39763fc239be7496d94f75fdea053746f0
Akira <tidemark0105@gmail.com>
2021-07-03 20:36:08 +0900

    add google service account api Why: to access google sheets How: Create google service account. Tags: Add
    Sponsored-by: Name <me@example.com>

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
 import os
+from google_test import google_account
 
 
 app = FastAPI()
@@ -16,6 +17,11 @@ async def index_page(request: Request):
 @app.get('/test')
 async def index():
     return { 'message': 'Hello World' }
+
+@app.get('/goog')
+async def goog():
+    res = google_account()
+    return res
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))

e387d742823c39eb6d3f77e4ff38f1ab9e2938d2
Mifuka Yumura <mifukuma-1st-323@outlook.jp>
2021-07-03 20:54:26 +0900

    POST処理テスト

--- app.py
+++ app.py
@@ -6,11 +6,25 @@ from pydantic import BaseModel
 import uvicorn
 import os
 
-class Item(BaseModel):
+class Student(BaseModel):
     name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
+    major: str
+    industry: str
+
+class User(BaseModel):
+    name: int
+    bf_industry_id: int
+    af_industry_id: int
+    bf_company_id: int
+    af_company_id: int
+
+class Post(BaseModel):
+    user_id: int
+    major: str
+    industry: str
+
+class Company(BaseModel):
+    name: str
 
 app = FastAPI()
 
@@ -23,9 +37,26 @@ async def index_page(request: Request):
 async def index():
     return { 'message': 'Hello World' }
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+@app.get('/students/{student_id}')
+async def index(student_id: 1):
+    sql="select * from Student where id=1"
+    return cursor.execute(sql)
+
+@app.get('/posts')
+async def index():
+    return student
+
+@app.post("/users")
+async def create_item(user: User):
+    return user
+
+@app.post("/posts")
+async def create_item(post: Post):
+    return post
+
+@app.post("/companies")
+async def create_item(company: Company):
+    return company
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))

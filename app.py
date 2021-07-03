8c75444523da58e276fa55f974b72121538a074a
Mifuka Yumura <mifukuma-1st-323@outlook.jp>
2021-07-03 20:08:56 +0900

    post テスト

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from typing import Optional
 from fastapi import FastAPI, Query, Path, Request 
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 import uvicorn
 import os
 
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 app = FastAPI()
 
@@ -16,6 +22,10 @@ async def index_page(request: Request):
 @app.get('/test')
 async def index():
     return { 'message': 'Hello World' }
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
